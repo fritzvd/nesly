255ffc7acc2a4e221ae3e877c4a0eb30868256d2
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2016-05-25 18:09:58 +0200

    Make palette functions that take arguments
    * standard formatting (almost there)
    * add addresses for ppu manipulation
    * make palette functions take palettes

--- index.js
+++ index.js
@@ -1,137 +1,147 @@
-var fs = require('fs');
-var falafel = require('falafel');
+var fs = require('fs')
+var falafel = require('falafel')
 
-var std = require('./lib/std');
-var header = require('./lib/header');
-var bank = require('./lib/banking');
-var palette = require('./lib/palettes');
-var sprites = require('./lib/sprites');
-var bg = require('./lib/bg.js');
-var joy = require('./lib/joy');
+var std = require('./lib/std')
+var header = require('./lib/header')
+var bank = require('./lib/banking')
+var palette = require('./lib/palettes')
+var sprites = require('./lib/sprites')
+var bg = require('./lib/bg.js')
+var joy = require('./lib/joy')
 
 var TOKENS = {
-    'reset': std.reset,
-    'vec': std.vec,
-    'vblankwait': std.vblankwait,
-    'clrmem': std.clrmem,
-    'header': header,
-    'bank': bank,
-    'palette': palette.palette,
-    'pData': palette.pData,
-    'addData': sprites.addData,
-    'loadSprites': sprites.loadSprites,
-    'sData': sprites.sData,
-    'setLowHighBytes': sprites.setLowHighBytes,
-    'moveRight': sprites.moveRight,
-    'moveLeft': sprites.moveLeft,
-    'moveUp': sprites.moveUp,
-    'moveDown': sprites.moveDown,
-    'enableBg': bg.enableBg,
-    'loadAttribute': bg.loadAttribute,
-    'addBg': bg.addBgRow,
-    'loadBg': bg.loadBg,
-    'write': bg.write,
-    'attributeTable': bg.attributeTable,
-    'loadNametable': bg.loadNametable,
-    'joyInit': joy.init,
-    'joyRead': joy.read,
-    'asm': function (exp) { return exp; }
-};
-
-function expressionStatement(node) {
-    var callee = node.expression.callee;
-    var args = [];
-    var name;
-    var fn;
-    var o;
-
-    var argumentFn = function (arg) {
-        var value;
-
-        if (arg.type === 'ObjectExpression') {
-            value = {};
-            arg.properties.forEach(function (p) {
-                var k = p.key.name;
-                var v;
-
-                if (p.value.type === 'Identifier') {
-                    v = TOKENS[p.value.name];
-                } else {
-                    v = p.value.value;
-                }
-
-                value[k] = v;
-                o = value;
-            });
-        } else {
-            if (arg.name) {
-                if (TOKENS[arg.name]) {
-                    value = TOKENS[arg.name];
-                }
-            } else {
-                value = arg.value;
-            }
-            args.push(value);
-        }
-    };
-
-    if (callee && callee.name) {
-        name = callee.name;
-    }
-
-    if (name) {
-        fn = TOKENS[name];
-        callee.parent.arguments.forEach(argumentFn);
-
-        if (fn) {
-            if (o) {
-                node.update(fn(o));
-                delete o;
-            } else {
-                node.update(fn(args));
-            }
-        }
-    }
+  'reset': std.reset,
+  'vec': std.vec,
+  'vblankwait': std.vblankwait,
+  'clrmem': std.clrmem,
+  'header': header,
+  'bank': bank,
+  'loadPalettes': palette.loadPalettes,
+  'setSpritePalette': palette.setSpritePalette,
+  'setBgPalette': palette.setBgPalette,
+  'pData': palette.pData,
+  'addData': sprites.addData,
+  'loadSprites': sprites.loadSprites,
+  'sData': sprites.sData,
+  'setLowHighBytes': sprites.setLowHighBytes,
+  'moveRight': sprites.moveRight,
+  'moveLeft': sprites.moveLeft,
+  'moveUp': sprites.moveUp,
+  'moveDown': sprites.moveDown,
+  'enableBg': bg.enableBg,
+  'loadAttribute': bg.loadAttribute,
+  'addBg': bg.addBgRow,
+  'loadBg': bg.loadBg,
+  'write': bg.write,
+  'attributeTable': bg.attributeTable,
+  'loadNametable': bg.loadNametable,
+  'joyInit': joy.init,
+  'joyRead': joy.read,
+  'asm': function (exp) { return exp; }
 }
 
-function variableDeclarator(node) {
-    var name = node && node.id && node.id.name;
-    var value = node && node.init && node.init.value;
-    var label = 'var_'+name+':\t';
+function expressionStatement (node) {
+  var callee = node.expression.callee
+  var args = []
+  var name
+  var fn
+  var o
 
-    var updatedValue;
+  var argumentFn = function (arg) {
+    var value
 
-    switch (value) {
-        case true:
-            updatedValue = 1;
-        break;
-        case false:
-            updatedValue = 0;
-        break;
-        default:
-            updatedValue = value;
+    if (arg.type === 'ArrayExpression' && callee.name.indexOf('Palette') > 0 ) {
+      value = ''
+
+      var original = arg.source()
+      value = original
+                .replace(/(\n*)(\[*)(\]*)(\ *)/g, '')
+                .replace(/0x/g, '$$')
+
+      args.push(value)
+    } else if (arg.type === 'ObjectExpression') {
+      value = {}
+      arg.properties.forEach(function (p) {
+        var k = p.key.name
+        var v
+
+        if (p.value.type === 'Identifier') {
+          v = TOKENS[p.value.name]
+        } else {
+          v = p.value.value
+        }
+
+        value[k] = v
+        o = value
+      })
+    } else {
+      if (arg.name) {
+        if (TOKENS[arg.name]) {
+          value = TOKENS[arg.name]
+        }
+      } else {
+        value = arg.value
+      }
+      args.push(value)
     }
+  }
 
-    if (node.parent && node.parent.kind === 'var') {
-        node.parent.update('');
+  if (callee && callee.name) {
+    name = callee.name
+  }
+
+  if (name) {
+    fn = TOKENS[name]
+    callee.parent.arguments.forEach(argumentFn)
+
+    if (fn) {
+      if (o) {
+        node.update(fn(o))
+      // delete o
+      } else {
+        node.update(fn(args))
+      }
     }
+  }
+}
 
-    node.update(label + '.db\t' + updatedValue);
+function variableDeclarator (node) {
+  var name = node && node.id && node.id.name
+  var value = node && node.init && node.init.value
+  var label = 'var_' + name + ':\t'
+
+  var updatedValue
+
+  switch (value) {
+    case true:
+      updatedValue = 1
+      break
+    case false:
+      updatedValue = 0
+      break
+    default:
+      updatedValue = value
+  }
+
+  if (node.parent && node.parent.kind === 'var') {
+    node.parent.update('')
+  }
+
+  node.update(label + '.db\t' + updatedValue)
 }
 
 var TYPES = {
-    'ExpressionStatement': expressionStatement,
-    'VariableDeclarator': variableDeclarator
-};
+  'ExpressionStatement': expressionStatement,
+  'VariableDeclarator': variableDeclarator
+}
 
-
-function compiler(node) {
-    if (TYPES[node.type]) {
-        TYPES[node.type](node);
-    }
+function compiler (node) {
+  if (TYPES[node.type]) {
+    TYPES[node.type](node)
+  }
 }
 
 module.exports.compile = function (file) {
-    var output = falafel(file, compiler);
-    return output;
-};
+  var output = falafel(file, compiler)
+  return output
+}
--- tests/bg.js
+++ tests/bg.js
@@ -1,10 +1,10 @@
-var test = require('tap').test;
-var bgLib = require('../lib/bg');
+var test = require('tap').test
+var bgLib = require('../lib/bg')
 
-test("make sure we write hello world", function (t) {
-    bgLib.write('hello world');
-    t.equal('\t.db 17,14,21,21,24,36,32,24,27,21,13,36,36,36,36,36', 
-            bgLib.background[0],
-            "Expected hello world");
-    t.end()
-});
+test('make sure we write hello world', function (t) {
+  bgLib.write('hello world')
+  t.equal('\t.db 17,14,21,21,24,36,32,24,27,21,13,36,36,36,36,36',
+    bgLib.background[0],
+    'Expected hello world')
+  t.end()
+})
