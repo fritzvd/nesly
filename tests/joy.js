72e216edc18394381257e49005f755906250c571
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2016-05-30 16:57:29 +0200

    standardizing

--- tests/joy.js
+++ tests/joy.js
@@ -5,10 +5,10 @@ test('make sure the controller works', function (t) {
   var error
 
   var expected = 'ReadA:\n\tlda $4016\n\tand #%00000001\n\tbeq ReadADone\ntest\nReadADone:'
-  var output = joyLib.read({label: 'ReadA', cb: function () { return 'test'; }})
+  var output = joyLib.read({label: 'ReadA', cb: function () { return 'test' }})
   t.equal(output, expected, 'ReadA did not produce the expected output')
 
-  output = joyLib.read({label: 'ReadB', cb: function () { return 'test'; }})
+  output = joyLib.read({label: 'ReadB', cb: function () { return 'test' }})
   expected = 'ReadB:\n\tlda $4016\n\tand #%00000001\n\tbeq ReadBDone\ntest\nReadBDone:'
   t.equal(output, expected, 'ReadB did not produce the expected output')
 
